72e7deb740a2446a21e36318e24c1702bc568294
Fyn_T <49595793+fyn2006@users.noreply.github.com>
2026-05-08 15:32:19 -0700

    Add envelope opening animation on button click

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const envelope = document.getElementById("envelope");
+const openBtn = document.getElementById("openBtn");
+const envelopeScreen = document.getElementById("envelopeScreen");
+const card = document.getElementById("card");
 
+openBtn.addEventListener("click", () => {
+  envelope.classList.add("open");
+
+  setTimeout(() => {
+    envelope.classList.add("fade");
+  }, 700);
+
+  setTimeout(() => {
+    envelopeScreen.classList.add("hidden");
+    card.classList.remove("hidden");
+  }, 1300);
+});
